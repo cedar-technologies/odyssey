9f642b3d7d9634dc21b1011e840265c5912b2e35
alex <jon@gmail.com>
2019-03-09 21:11:41 -0800

    updated details on who

--- frontend/src/app/care-plan/mock-practicians.ts
+++ frontend/src/app/care-plan/mock-practicians.ts
@@ -14,12 +14,52 @@ export const PRACTICIANS : Practician[] = [
       },
       {
         id: 2,
+        name: "Pharmacist",
+        what: "Every time you have your prescription filled there is a series of steps your pharmacist  will perform: First your pharmacist will ask for your name, your British Columbia CareCard and one other piece of identification. Your pharmacist reviews your prescription to make sure: The medication, strength, and dosage instructions are appropriate for your health condition. The new prescription is okay to take with other medications you may be taking. There are no potential problems such as duplicate therapy or drug interactions. Your pharmacist will provide counseling on any new medication and will confirm/explain all details and instructions.",
+        how: "You can connect with your pharmacist by phoning your local pharmacy, or by visiting in person. ",
+        when: "At any local pharmacy, whenever they are open.",
+        where: "",
+        who: "Pharmacists are medication experts.  They work to make sure you get the right amount of medications at the right times, with the right instructions.  They are a great source of information for answering any questions regarding the medications you have been prescribed.",
+        img: "pharmacist.jpg"
+      },
+      {
+        id: 2,
+        name: "Laboratory",
+        what: "",
+        how: "Laboratory services are responsible for 80%( check this) of your diagnosis.  This is achieved by following a series of steps to reach your goal of a diagnosis.The first step involves collection of samples.  This includes the collection of blood and other various body fluids, tissues and cells samples collected by a doctor/nurse in an outpatient or inpatient setting",
+        when: "",
+        where: "There are many places you can go to get your blood drawn.  Most communities have a variety of private and public labs.  These include outpatient labs connected to the hospitals or a Satellite Lab. ",
+        who: "There are many types workers that make up a laboratory. The Phlebotomist.  They perform blood draws and prepare samples for testing.  They also enter test for testing, give out collection information and prepare collection kits for you to take home. Laboratory Technologist work together with many disciplines in the core lab to test samples and help patients receive the care they need.  Laboratory Doctors and Pathologists diagnose and characterize disease by examining a patient’s tissues, blood, and other body fluids. ",
+        img: "laboratory.jpg"
+      },
+      {
+        id: 2,
         name: "Primary Care Provider",
-        what: "You will most likely see your primary care provider in their office.  Appointment lengths are based on your reason for visit.  They will review with you your reason for visit, and may take time to review your current health status and medications.  Your visit may include a routine physical assessment such as checking your blood pressure, weight, or listening to breathing with a stethoscope.",
-        how: "Write down your questions ahead of time, listing your symptoms and concerns Bring along a list of any prescription or non-prescription (over the counter) medications that you take. Have a support person come along.",
-        when: "Your primary care provider (PCP) should be your first call in non-emergency situations including prescription refills. You should also make an appointment with your PCP to follow-up after any visit to the Emergency Department or after an admission to the hospital. If you are receiving specialist treatment, such as cancer treatment, it is important to continue to see your PCP as they will continue to manage your general health care after your specialist treatments are over.",
-        where: "If you do not already have a Primary Health Care Provider: Contact HealthLink BC using the 8-1-1 toll-free number (If you are hearing impaired, call 7-1-1.). You will be able to speak to a health service navigator who can provide you with information about finding a family doctor in your own community. Ask your family or friends to introduce you to their own family doctor. Sometimes a doctor may take you on referral from an existing patient. If you are visiting another health care provider, such as a specialist, ask them if they know of any family doctors that are accepting patients. If you are visiting a walk-in clinic, ask the doctor if they would be willing to take you on as a patient.",
-        who: "Your primary care provider (PCP) could be your family doctor, GP (general practitioner) or nurse practitioner. Your PCP is at the center of managing your general health care, with a focus on your wellness and quality of life, as well as your physical or mental health concerns.  PCPs can also make referrals to specialists when required.",
+        what: "",
+        how: "",
+        when: "",
+        where: "",
+        who: "",
+        img: "diagnostic_imaging.jpg"
+      },
+      {
+        id: 2,
+        name: "Primary Care Provider",
+        what: "",
+        how: "",
+        when: "",
+        where: "",
+        who: "",
+        img: "diagnostic_imaging.jpg"
+      },
+      {
+        id: 2,
+        name: "Primary Care Provider",
+        what: "",
+        how: "",
+        when: "",
+        where: "",
+        who: "",
         img: "diagnostic_imaging.jpg"
       }
 ]
