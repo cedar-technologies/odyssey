7b9d9472a7988ef9f543e7f148f142b28801f14b
alex <jon@gmail.com>
2019-03-09 20:45:55 -0800

    work on the pages

---
+++ frontend/src/app/care-plan/mock-practicians.ts
@@ -0,0 +1,25 @@
+import { Practician } from './practician';
+
+
+export const PRACTICIANS : Practician[] = [
+    {
+        id: 1,
+        name: "Primary Care Provider",
+        what: "You will most likely see your primary care provider in their office.  Appointment lengths are based on your reason for visit.  They will review with you your reason for visit, and may take time to review your current health status and medications.  Your visit may include a routine physical assessment such as checking your blood pressure, weight, or listening to breathing with a stethoscope.",
+        how: "Write down your questions ahead of time, listing your symptoms and concerns Bring along a list of any prescription or non-prescription (over the counter) medications that you take. Have a support person come along.",
+        when: "Your primary care provider (PCP) should be your first call in non-emergency situations including prescription refills. You should also make an appointment with your PCP to follow-up after any visit to the Emergency Department or after an admission to the hospital. If you are receiving specialist treatment, such as cancer treatment, it is important to continue to see your PCP as they will continue to manage your general health care after your specialist treatments are over.",
+        where: "If you do not already have a Primary Health Care Provider: Contact HealthLink BC using the 8-1-1 toll-free number (If you are hearing impaired, call 7-1-1.). You will be able to speak to a health service navigator who can provide you with information about finding a family doctor in your own community. Ask your family or friends to introduce you to their own family doctor. Sometimes a doctor may take you on referral from an existing patient. If you are visiting another health care provider, such as a specialist, ask them if they know of any family doctors that are accepting patients. If you are visiting a walk-in clinic, ask the doctor if they would be willing to take you on as a patient.",
+        who: "Your primary care provider (PCP) could be your family doctor, GP (general practitioner) or nurse practitioner. Your PCP is at the center of managing your general health care, with a focus on your wellness and quality of life, as well as your physical or mental health concerns.  PCPs can also make referrals to specialists when required.",
+        img: "primary_care.jpg"
+      },
+      {
+        id: 2,
+        name: "Primary Care Provider",
+        what: "You will most likely see your primary care provider in their office.  Appointment lengths are based on your reason for visit.  They will review with you your reason for visit, and may take time to review your current health status and medications.  Your visit may include a routine physical assessment such as checking your blood pressure, weight, or listening to breathing with a stethoscope.",
+        how: "Write down your questions ahead of time, listing your symptoms and concerns Bring along a list of any prescription or non-prescription (over the counter) medications that you take. Have a support person come along.",
+        when: "Your primary care provider (PCP) should be your first call in non-emergency situations including prescription refills. You should also make an appointment with your PCP to follow-up after any visit to the Emergency Department or after an admission to the hospital. If you are receiving specialist treatment, such as cancer treatment, it is important to continue to see your PCP as they will continue to manage your general health care after your specialist treatments are over.",
+        where: "If you do not already have a Primary Health Care Provider: Contact HealthLink BC using the 8-1-1 toll-free number (If you are hearing impaired, call 7-1-1.). You will be able to speak to a health service navigator who can provide you with information about finding a family doctor in your own community. Ask your family or friends to introduce you to their own family doctor. Sometimes a doctor may take you on referral from an existing patient. If you are visiting another health care provider, such as a specialist, ask them if they know of any family doctors that are accepting patients. If you are visiting a walk-in clinic, ask the doctor if they would be willing to take you on as a patient.",
+        who: "Your primary care provider (PCP) could be your family doctor, GP (general practitioner) or nurse practitioner. Your PCP is at the center of managing your general health care, with a focus on your wellness and quality of life, as well as your physical or mental health concerns.  PCPs can also make referrals to specialists when required.",
+        img: "diagnostic_imaging.jpg"
+      }
+]
--- frontend/src/app/care-plan/practician.ts
+++ frontend/src/app/care-plan/practician.ts
@@ -1,8 +1,10 @@
 export class Practician {
+    id: number;
     name: string;
     who: string;
     where: string;
     when: string;
     how: string;
     what: string;
+    img: string;
   }
